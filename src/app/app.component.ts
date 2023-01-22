d8f491cdefcc02a2a2331c445e97f621a1eee812
abdelghafour ettaqui <ettaqui88@gmail.com>
2023-01-22 13:50:12 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Banque_centrale_du_Maroc_front-end';
+}

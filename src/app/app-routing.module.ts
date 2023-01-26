b5d39260d5cfe3cd4579d263253877e12ca87329
Moubtahij Otmane <otman.moubtahij15@gmail.com>
2023-01-26 10:45:30 +0100

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from "./Pages/login/login.component";
 import {RegisterComponent} from "./Pages/register/register.component";
 import {CommonModule} from "@angular/common";
 
 const routes: Routes = [
-  {path: '',redirectTo:'/login',pathMatch:'full' },
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
   {
     path: "login",
-    component: LoginComponent
+    component: LoginComponent,
   },
   {
     path: "register",
@@ -24,4 +24,5 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
